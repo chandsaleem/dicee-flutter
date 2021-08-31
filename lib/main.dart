4e1872f74c2431fac2a3e8e5ff0e0f28fae12c83
Chand Saleem <84383282+chandsaleem@users.noreply.github.com>
2021-08-31 22:54:38 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.pink.shade200,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.pink,
         ),
         body: DicePage(),
       ),
